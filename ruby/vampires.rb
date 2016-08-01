efde9f166f68f1776ad23564229931a9549ebdbe
Jesusmar Lima <jesusmarlima@gmail.com>
2016-08-01 16:45:08 -0400

    add questions to the vamp detector form

---
+++ ruby/vampires.rb
@@ -0,0 +1,10 @@
+p "What is your name ? "
+name = gets.chomp()
+p "How old are you ? "
+age = gets.chomp()
+p "what year were you born ? "
+year_born = gets.chomp()
+p "Our company cafeteria serves garlic bread. Should we order some for you? "
+garlic_bread = gets.chomp()
+p "Would you like to enroll in the company’s health insurance?"
+health_insurance = gets.chomp()
