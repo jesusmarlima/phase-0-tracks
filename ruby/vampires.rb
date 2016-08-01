2352b6b2dc8db494c55475c3ec2cf020bcb9381f
Jesusmar Lima <jesusmarlima@gmail.com>
2016-08-01 18:58:36 -0400

    add spaces to looks good

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,27 +1,29 @@
-p "How many employees will be processed ? "
+print "How many employees will be processed ? "
 number_of_employees = gets.chomp().to_i
 times = 0
 
 while times < number_of_employees do
-    p "What is your name ? "
+    puts " "
+    print "What is your name ?   "
     name = gets.chomp()
-    p "How old are you ? "
+    print "How old are you ?   "
     age = gets.chomp()
-    p "what year were you born ? "
+    print "what year were you born ?   "
     year_born = gets.chomp()
-    p "Our company cafeteria serves garlic bread. Should we order some for you? "
-    eat_garlic_bread = gets.chomp().do == "yes" ? true : false
-    p "Would you like to enroll in the company’s health insurance?"
+    print "Our company cafeteria serves garlic bread. Should we order some for you?   "
+    eat_garlic_bread = gets.chomp().downcase == "yes" ? true : false
+    print "Would you like to enroll in the company’s health insurance?   "
     want_health_insurance = gets.chomp().downcase == "yes" ? true : false
     age_ok = (age.to_i + year_born.to_i) == 2016
 
     allergy = ""
     until allergy.downcase == "done" do
-      p "name any allergies to you have:"
+      print "name any allergies to you have:"
       allergy = gets.chomp
       break if allergy == "sunshine"
     end
 
+    puts " "
     if allergy == "sunshine"
       puts "Probably a vampire."
     elsif age_ok && ( eat_garlic_bread || want_health_insurance )
@@ -38,5 +40,5 @@ while times < number_of_employees do
 
     times += 1
 end
-
+puts " "
 puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
