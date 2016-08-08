07c8c0e3424977905a5680b752c2853f5524442d
Jesusmar Lima <jesusmarlima@gmail.com>
2016-08-08 15:18:13 -0400

    Print the hash back out to the screen when the designer has answered all of the questions.

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -4,6 +4,8 @@
 
 #convert variables to the right type
 
+#print out all the answers
+
 Gem.win_platform? ? (system "cls") : (system "clear")
 
 hash = {}
@@ -13,16 +15,33 @@ hash[:name] = gets.chomp
 print "Client age:"
 hash[:age] = gets.chomp.to_i
 print "Number of childreen:"
-hash[:child_number] = gets.chomp.to_i
+hash[:'childreen number'] = gets.chomp.to_i
 print "Deccor Theme:"
-hash[:decor_theme] = gets.chomp
+hash[:'decor theme'] = gets.chomp
 print "is accessibility needed? (y/n)"
 hash[:accessibility] = gets.chomp == "y" ? true : false
 if hash[:accessibility]
   print "accessibility needs:"
-  hash[:accessibility_kind] = gets.chomp
+  hash[:'accessibility needs'] = gets.chomp
 end
 
+puts " "
+puts "Answers:"
+puts " "
+
+def bool_to_s(bool)
+  if bool.to_s == "true"
+    "yes"
+  elsif bool.to_s == "false"
+    "No"
+  else
+    bool
+  end
+end
+
+hash.each do |k,v|
+   puts "#{k.to_s.capitalize}: #{bool_to_s(v)}"
+end
 
 
 
