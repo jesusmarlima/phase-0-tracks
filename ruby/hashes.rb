4bd593d1c7517a7e053f1b1ae563541a5ad6539b
Jesusmar Lima <jesusmarlima@gmail.com>
2016-08-08 15:00:40 -0400

    Convert any user input to the appropriate data type.

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -2,6 +2,8 @@
 #hash variable to store the keys and values
 #ask user the questions and store in the hash
 
+#convert variables to the right type
+
 Gem.win_platform? ? (system "cls") : (system "clear")
 
 hash = {}
@@ -9,14 +11,14 @@ hash = {}
 print "Client name:"
 hash[:name] = gets.chomp
 print "Client age:"
-hash[:age] = gets.chomp
+hash[:age] = gets.chomp.to_i
 print "Number of childreen:"
-hash[:child_number] = gets.chomp
+hash[:child_number] = gets.chomp.to_i
 print "Deccor Theme:"
 hash[:decor_theme] = gets.chomp
 print "is accessibility needed? (y/n)"
-hash[:accessibility] = gets.chomp
-if hash[:accessibility] == "y"
+hash[:accessibility] = gets.chomp == "y" ? true : false
+if hash[:accessibility]
   print "accessibility needs:"
   hash[:accessibility_kind] = gets.chomp
 end
