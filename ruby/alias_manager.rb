0f0acac53663fd2383b5b0a6efd65f7e0c177e04
Jesusmar Lima <jesusmarlima@gmail.com>
2016-08-09 18:38:29 -0400

    initial commit, release 0 alg on pseudocode

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,16 @@
+# given a full name
+# store full in the variable
+# split the string into array
+# revert the array using bang!
+# join in a string again
+# new string is needed to store the new values
+# "aeiou" string is needed to know if each char of the array of strings is vowel or consonants
+# loop into the each char of the string
+# ask if is vowel
+# if is true get next element of vowels
+# if is consonant get next consonant
+# print the consonant at the end
+
+
+
+
