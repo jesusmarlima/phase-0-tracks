e27d1282894b550da3de4c93634d09ba67e7b811
Jesusmar Lima <jesusmarlima@gmail.com>
2016-08-09 18:55:56 -0400

    add method next_char

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,7 +1,7 @@
 # given a full name
 # store full in the variable
 # split the string into array
-# revert the array using bang!
+# revert the array
 # join in a string again
 # new string is needed to store the new values
 # "aeiou" string is needed to know if each char of the array of strings is vowel or consonants
@@ -10,6 +10,29 @@
 # if is true get next element of vowels
 # if is consonant get next consonant
 # print the consonant at the end
+
+def next_char(char)
+  vowels = "aeiou"
+  if char == " "
+      char
+  elsif vowels.include?(char)
+      get_next_vowel(char)
+  else
+      get_next_consonat(char)
+  end
+end
+
+def alias_to(agent_name)
+  names = agent_name.split(" ")
+  names[0],names[1] = names[1],names[0]
+  inverted_names = names.join(" ")
+  agent_alias = ""
+  inverted_names.chars.each do |char|
+      agent_alias << next_char(char)
+  end
+  agent_alias
+end
+puts alias_to("marco aurelio")
 
 
 
