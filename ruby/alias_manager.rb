2cdc153ae5ffbe319c919aea707721f724623810
Jesusmar Lima <jesusmarlima@gmail.com>
2016-08-09 20:11:40 -0400

    add comments change the invert method to suport more then 2 names final commit release 1

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -11,6 +11,12 @@
 # if is consonant get next consonant
 # print the consonant at the end
 
+# clear screen
+def clear_screen
+  Gem.win_platform? ? (system "cls") : (system "clear")
+end
+
+#this method return the next vowel
 def get_next_vowel(vowel,vowels)
   next_vowel = vowel
   loop do
@@ -20,6 +26,7 @@ def get_next_vowel(vowel,vowels)
   next_vowel[-1]
 end
 
+#this method return the next consonant
 def get_next_consonant(consonant,vowels)
    next_consonant = consonant
   loop do
@@ -29,13 +36,16 @@ def get_next_consonant(consonant,vowels)
   next_consonant[-1]
 end
 
-def is_capitalized?(char)
+#this method return id char is down or uppcase
+def is_upcase?(char)
   downcased_char = char.downcase
   downcased_char != char
 end
 
+#this method return next char of vowel or consonant, and space if the char is space
 def next_char(char)
-  vowels = is_capitalized?(char) ? "AEIOU" : "aeiou"
+  #if the char is capitalized we have to ask for uppercased vowels
+  vowels = is_upcase?(char) ? "AEIOU" : "aeiou"
   if char == " "
       char
   elsif vowels.include?(char)
@@ -45,22 +55,36 @@ def next_char(char)
   end
 end
 
+# this method invert the agent name
+def invert_names(agent_name)
+  inverted_names = []
+  agent_name.split(" ").each do |name|
+    inverted_names.unshift(name)
+  end
+  inverted_names.join(" ")
+end
+
+#this method do what is asked (main method)
 def alias_to(agent_name)
-  names = agent_name.split(" ")
-  names[0],names[1] = names[1],names[0]
-  inverted_names = names.join(" ")
+  #inverting names
+  inverted_names = invert_names(agent_name)
   agent_alias = ""
+  #for each char of the inverted names change to the next char if necessary
   inverted_names.chars.each do |char|
       agent_alias << next_char(char)
   end
   agent_alias
 end
 
-# puts alias_to("Marco Aurelio") == "Easimou Nesdu"
-# puts alias_to("Jesusmar Lima") == "Mone Kitatnes"
-
 
 #tests
+
+# puts alias_to("Marco Aurelio") == "Easimou Nesdu"
+# puts alias_to("Jesusmar Lima") == "Mone Kitatnes"
+# puts alias_to("JeSuSmar PeReIra LimA") == "MonE QiSiOse KiTaTnes"
+# puts alias_to("JeSuSmar PeReIra LimA FilHO") == "GomJU MonE QiSiOse KiTaTnes"
+
+
 
 # puts get_next_vowel("a","aeiou") == "e"
 # puts get_next_vowel("e","aeiou") == "i"
@@ -70,3 +94,17 @@ end
 # puts get_next_consonant("d","aeiou") == "f"
 # puts get_next_consonant("x","aeiou") == "y"
 # puts get_next_consonant("z","aeiou") == "b"
+
+
+#-------------BEGIN PROGRAM------------------------------
+
+clear_screen
+loop do
+  puts " "
+  print "Dear mr(s) agent, please type your full name or 'quit' to exit :"
+  full_name = gets.chomp
+  break if full_name == "quit"
+  puts " "
+  print "Mr(s) #{full_name} your alias is: #{alias_to(full_name)}"
+  puts " "
+end
