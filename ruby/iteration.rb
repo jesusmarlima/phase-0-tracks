e190b9b55594f62e15a0fd32a29d2e50b4d9f1a6
Jesusmar Lima <jesusmarlima@gmail.com>
2016-08-10 17:11:36 -0400

    add block parameters - release 0

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,6 +1,11 @@
+# this method just print the names of the students and the last name on the block that is passed.
 def print_names
-  puts "Jesus"
-  puts "Jesusmar"
+  last_name = "Zambrano"
+  print "Jesus"
+  yield (last_name)
+  last_name = "Lima"
+  print "Jesusmar"
+  yield (last_name)
 end
 
-print_names
+print_names { |last_name| puts " last name is #{last_name}" }
