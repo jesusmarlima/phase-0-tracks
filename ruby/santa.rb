6659dd2c357a6289c0e5bfa02fd3878cc6ed0fbf
Jesusmar Lima <jesusmarlima@gmail.com>
2016-08-14 23:25:47 -0400

    add a bunch of instaces of santas

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,6 +1,10 @@
 class Santa
 
-def initialize
+def initialize(gender,ethnicity)
+  @gender = gender
+  @ethnicity = ethnicity
+  @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+  @age = 0
   puts "\ninitializing Santa instance..."
 end
 
@@ -14,8 +18,32 @@ end
 
 end
 
-santa = Santa.new
+# santa = Santa.new
+# santa.eat_milk_and_cookies("crakers")
+# santa.speak
 
-santa.eat_milk_and_cookies("crakers")
+santas = []
 
-santa.speak
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+
+#sorting genders array by alphabet
+example_genders.sort!
+#sorting ethnicities array by alphabet revesed
+example_ethnicities.sort!{|a , b| b <=> a}
+
+#initializing with gender and ethnicities mixed
+example_genders.each_with_index do |gender,index|
+  santas << Santa.new(gender,example_ethnicities[index])
+end
+
+puts " ---------------------------------------------------- "
+#sorting ethnicities array by third letter.
+example_genders.sort!{|a , b| b[2] <=> a[2]}
+#sorting genders array by alphabet
+example_ethnicities.sort!
+
+#initializing with gender and ethnicities mixed
+example_genders.each_with_index do |gender,index|
+  santas << Santa.new(gender,example_ethnicities[index])
+end
