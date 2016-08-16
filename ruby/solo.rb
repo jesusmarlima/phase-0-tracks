6b02d34e2227ea12564ef824130ede56a7cda16e
Jesusmar Lima <jesusmarlima@gmail.com>
2016-08-16 13:15:10 -0400

    initial commit, release 0 solo challenge 6.4

---
+++ ruby/solo.rb
@@ -0,0 +1,15 @@
+# class soocer player
+
+# CHARACTERISTICS ----------------
+# name: varies
+# age varies
+# salary: number
+# number 0f games played
+# gols amount
+# sex
+
+# BEHAVIOR -----------------------
+# run
+# shoot
+# pass
+# celebrate gols
