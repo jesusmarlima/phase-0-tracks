eb9e635edd60819b21f3dd1a441025daaa7451e8
Jesusmar Lima <jesusmarlima@gmail.com>
2016-08-16 17:43:13 -0400

    add release 3, convert stand alone to mixin module

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,14 +1,53 @@
 module Shout
 
-  def self.yell_angrily(words)
+  # def self.yell_angrily(words)
+  #   words + "!!!" + " :("
+  # end
+
+  # def self.yelling_happily(words)
+  #   words + "!!!!" + " :DDDD"
+  # end
+
+  def yell_angrily(words)
+    yield
     words + "!!!" + " :("
   end
 
-  def self.yelling_happily(words)
+  def yelling_happily(words)
+    yield
     words + "!!!!" + " :DDDD"
   end
 
+
+
 end
 
-puts Shout.yelling_happily("testando o modulo")
-puts Shout.yell_angrily("testando o modulo")
+# puts Shout.yelling_happily("testando o modulo")
+# puts Shout.yell_angrily("testando o modulo")
+
+
+class Woman
+  include Shout
+end
+
+class Man
+  include Shout
+end
+
+class Child
+  include Shout
+end
+
+#driver code
+
+woman = Woman.new
+man = Man.new
+child = Child.new
+
+puts woman.yelling_happily("lets code!!!") {puts woman.class }
+puts man.yelling_happily("lets go to the bar!!!") {puts man.class}
+puts child.yelling_happily("lets play!!!") {puts child.class }
+
+puts child.yell_angrily("I don't want sleep") {puts child.class }
+puts man.yell_angrily("Crap, I forgot my cellphone at home") {puts man.class }
+puts woman.yell_angrily("haaaa, I tore my pantyhose") {puts woman.class }
