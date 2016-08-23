05f8ed7aab81b08425dacf4e127e4649eb06985e
Jesusmar Lima <jesusmarlima@gmail.com>
2016-08-23 18:18:56 -0400

    add pseudocode to revert strings

---
+++ js/explore.js
@@ -0,0 +1,6 @@
+// given a string print reversed
+
+// Create a method to reverse a given string
+// acess each char with the index from the end of the string to the start (string[9],string[8]...)
+// add each char to a new string
+//return a string
