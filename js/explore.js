bba52caf2ba2b544149fab54a1871514f40f473f
Jesusmar Lima <jesusmarlima@gmail.com>
2016-08-23 18:25:25 -0400

    add function reverse and test

--- js/explore.js
+++ js/explore.js
@@ -3,4 +3,16 @@
 // Create a method to reverse a given string
 // acess each char with the index from the end of the string to the start (string[9],string[8]...)
 // add each char to a new string
-//return a string
+//return a new string
+
+function reverse(string) {
+
+  var reversed = "";
+  for (i=0;i<string.length;i++){
+    index_reversed = (string.length - 1) - i;
+    reversed += string[index_reversed];
+  }
+  return reversed
+};
+
+console.log("mario reverted is :",reverse("mario"));
