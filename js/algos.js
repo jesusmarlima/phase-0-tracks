f10f152e4c0c9f9d2818abb96ae7a5a902f4fa6f
Jesusmar Lima <jesusmarlima@gmail.com>
2016-08-24 10:28:32 -0400

    add pseudocode and code for  7.3 Solo Challenge - Release 1

--- js/algos.js
+++ js/algos.js
@@ -4,6 +4,7 @@
 // save the index of the longest word or phrase.
 // access the array at this index and return the longest word/phrase
 
+//-------------------- release 0--------------------------------
 function longest_word_or_phrase(phrases){
   longest_word_index = 0;
   for (i=0; i < (phrases.length - 1) ; i++){
@@ -14,6 +15,46 @@ function longest_word_or_phrase(phrases){
   return phrases[longest_word_index];
 };
 
+//-------------------- release 1--------------------------------
 
+// given to objects compare if they are at least one attribute key and value.
+// loop through the fist object and ask if in the second object has the same pair key and value.
+//I chose to do with to methods to leave the code readable
+
+function key_value_match(obj1,obj2){
+
+  for (var attribute in obj1){
+      if (has_key_value(attribute,obj1[attribute],obj2)){
+        return true;
+      }
+  }
+  return false;
+
+};
+
+function has_key_value(key,value,obj2){
+  for (var attribute in obj2){
+    if (attribute === key && value === obj2[attribute]){
+      return true;
+    }
+  }
+  return false;
+};
+
+//------------------------ driver code -------------------------------------
+//test release 0
 array = ["me","I am the king of the word","Titanic","how about to see a movie tomorrow"]
 console.log("longest phrases or word is: " , longest_word_or_phrase(array) );
+
+
+//test release 1
+
+var pear = {color: 'green', weight:10}
+var apple = {color: 'red', weight:10}
+
+if (key_value_match(pear,apple)){
+  console.log("they have at least one common attribute")
+  }
+else {
+  console.log("Nothing equals");
+}
