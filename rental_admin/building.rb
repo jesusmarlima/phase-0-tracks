f167e5eae60986d26d365e841be94f05d4e6c7c0
Jesusmar Lima <jesusmarlima@gmail.com>
2016-09-02 15:40:49 -0400

    add comments to building class

--- rental_admin/building.rb
+++ rental_admin/building.rb
@@ -15,12 +15,14 @@ class Building
     end
   end
 
+  # this method is used to persist a Building into database
   def create
       @db.execute(SQL_INSERT_BUILDING,[@name,@floors,@apts_by_floor]);
       @id = @db.last_insert_row_id
       self
   end
 
+  #clear all builds (used so far only in tests)
   def clear
      @db.execute(SQL_CLEAR_TESTS_BUILDING_APARTMENTS,self.id)
      @db.execute(SQL_CLEAR_TESTS_BUILDING,self.id)
@@ -30,6 +32,7 @@ class Building
     @name
   end
 
+  # this methods returns an array of apartments of this instace o building
   def apartments
     apartments_result_set = @db.execute(SQL_SELECT_APARTMENTS_FROM_BUILDING,@id)
     apartments = []
@@ -40,9 +43,16 @@ class Building
     apartments
   end
 
+  # this method inverts the apartments on the builds to show at print
+  # I don't fell good leaving this here, I think is the better place is at # Helper module, but I am usging the instance of this each building.
+  # in: [[1A,1B,1C][2A,2B,2C]]
+  # out: [[2A,2B,2C][1A,1B,1C]]
+
   def all_apartments_in_floors
     floors = []
+    # slice the apartments in floors
     apartments_by_floor = self.apartments.each_slice(self.apts_by_floor)
+    # invert the floor to a better look when is printed
     (self.floors).downto(1).each do |floor_number|
       apartments_in_floor = apartments_by_floor.entries[floor_number - 1]
       floors << apartments_in_floor.map{|apt| apt}
