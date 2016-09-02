7b9387c0cd7b0774e96f4f29edd56f1029c65778
Jesusmar Lima <jesusmarlima@gmail.com>
2016-09-02 15:45:24 -0400

    delete building_rspec because i think in fact I am testing re rental administration

--- rental_admin/rental_administration_rspec.rb
+++ rental_admin/rental_administration_rspec.rb
@@ -29,6 +29,23 @@ describe 'rental_administration' do
     expect(@building.apartments.length).to eq 20
   end
 
+  it "prints building with one floor and 5 apts" do
+    rental.initial_setup
+    building = rental.create_building(nil, "New Buidling", 1, 3)
+    apartments = building.all_apartments_in_floors[0].map(&:number)
+    expect(apartments).to eq  ["1A","1B","1C"]
+  end
+
+  it "prints building with 2 floor and 5 apts" do
+    rental.initial_setup
+    building = rental.create_building(nil, "New Buidling", 2, 2)
+    floor2 = building.all_apartments_in_floors[0].map(&:number)
+    floor1 = building.all_apartments_in_floors[1].map(&:number)
+    apartments = [floor2,floor1]
+    expect(apartments).to eq [["2A","2B"], ["1A","1B"]]
+  end
+
+
   # this code clean the database tests
   after(:example) do
     @building.clear if @building
