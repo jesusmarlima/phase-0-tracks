a3b26c5b1b558048345e6fb8990ef930058238c1
Jesusmar Lima <jesusmarlima@gmail.com>
2016-09-02 16:23:44 -0400

    add comment to the helper file, refacoting format method helper

--- rental_admin/helper.rb
+++ rental_admin/helper.rb
@@ -1,5 +1,8 @@
 module Helper
 
+    #this method return a letter or letters to compose the apartment number
+    # in: index to find the letter
+    # out:  the letter of the alphabet that index match, if the index is out of bounds, I am using the next method in the 'Z' char so 'AA',AB...
     def self.getLetter(number)
      letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
      last_letter = letters[-1]
@@ -14,15 +17,12 @@ module Helper
      last_letter
   end
 
+  # this method formats what is printing '| 1A |' because if the apartment # has two numbers or two letters the building turn unaligned
+  # there is a limit to stay align = 6.
   def self.format(apartment)
-    leng = apartment.number.length
-    if leng == 2
-      " |  #{apartment}  | "
-    elsif leng == 3
-      " | #{apartment}  | "
-    else
-      " |#{apartment}  | "
-    end
+    begin_str = " |"
+    end_str = "| "
+    result = begin_str + apartment.number.center(6," ") + end_str
   end
 
 
