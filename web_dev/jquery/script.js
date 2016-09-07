6f7c64e65b45559dd1e9ba97917d9d1c6872b6c6
Jesusmar Lima <jesusmarlima@gmail.com>
2016-09-07 13:56:58 -0400

    add eventlisteners to change the opacity when mouse pass over the text

--- web_dev/jquery/script.js
+++ web_dev/jquery/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 
+     //array with some photo paths and index variable to control what will be shown
      var index = 0;
      var srcs = ["images/thumb_IMG_1992_1024.jpg","images/thumb_IMG_1186_1024.jpg",
                 "images/thumb_IMG_2420_1024.jpg","images/thumb_IMG_1725_1024.jpg",
@@ -7,6 +8,7 @@ $(document).ready(function(){
                 "images/thumb_IMG_2422_1024.jpg","images/thumb_IMG_1633_1024.jpg"]
 
 
+    // script adds EventListener (click) and change the photos to the left side
      $("#prev").click(function(event){
          index = index - 1;
           if (index < 0){
@@ -15,11 +17,27 @@ $(document).ready(function(){
         $("#img_master").attr("src",srcs[index])
      });
 
+
+    // script adds EventListener (click) and change the photos to the right side
       $("#next").click(function(event){
            index = index + 1;
            if (index > srcs.length){
              index = 0;
            }
         $("#img_master").attr("src",srcs[index])
+     });
+
+    // script adds EventListeners to change opacity when the mouse pass over the text
+    $("#prev").mouseenter(function(event){
+        event.target.style.opacity = 1
+     });
+    $("#prev").mouseleave(function(event){
+        event.target.style.opacity = 0.5
+     });
+    $("#next").mouseenter(function(event){
+        event.target.style.opacity = 1
+     });
+    $("#next").mouseleave(function(event){
+        event.target.style.opacity = 0.5
      });
 });
