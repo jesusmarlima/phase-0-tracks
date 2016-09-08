b9243b959d05bd11781276bd715cbfbca82e74a6
Jesusmar Lima <jesusmarlima@gmail.com>
2016-09-08 09:33:42 -0400

    refactoring to use sql_schema.rb

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -1,28 +1,11 @@
 # require gems
 require 'sinatra'
 require 'sqlite3'
-#require_relative 'sql_schema'
+require_relative 'sql_schema'
 
 set :public_folder, File.dirname(__FILE__) + '/static'
 
 db = SQLite3::Database.new("students.db")
-SQL_CREATE_CAMPUSES = <<-SQL
-CREATE TABLE IF NOT EXISTS campuses (
-    id INTEGER PRIMARY KEY,
-    name VARCHAR(255)
-  )
-SQL
-
-SQL_CREATE_CAMPUSES_STUDENTS = <<-SQL
-  CREATE TABLE IF NOT EXISTS campuses_students (
-    id INTEGER PRIMARY KEY,
-    student_id INTEGER,
-    campus_id INTEGER,
-    FOREIGN KEY (student_id) REFERENCES students(id),
-    FOREIGN KEY (campus_id) REFERENCES campuses(id)
-  )
-SQL
-
 db.execute(SQL_CREATE_CAMPUSES)
 db.execute(SQL_CREATE_CAMPUSES_STUDENTS)
 db.results_as_hash = true
@@ -91,11 +74,6 @@ end
 
 #get route from campuses_students called allocations
 get '/allocations' do
-  SQL_ALLOCATIONS = <<-SQL
-    SELECT s.name student, c.name campus FROM campuses c, students s, campuses_students cs
-      WHERE c.id = cs.campus_id
-      AND   cs.student_id = s.id
-  SQL
   @allocations = db.execute(SQL_ALLOCATIONS)
   erb :allocations
 end
--- web_dev/sinatra_templating/sql_schema.rb
+++ web_dev/sinatra_templating/sql_schema.rb
@@ -1,12 +1,11 @@
-
-SQL_CREATE_CAMPUSES <<-SQL
+SQL_CREATE_CAMPUSES = <<-SQL
 CREATE TABLE IF NOT EXISTS campuses (
     id INTEGER PRIMARY KEY,
     name VARCHAR(255)
   )
 SQL
 
-SQL_CREATE_CAMPUSES_STUDENTS <<-SQL
+SQL_CREATE_CAMPUSES_STUDENTS = <<-SQL
   CREATE TABLE IF NOT EXISTS campuses_students (
     id INTEGER PRIMARY KEY,
     student_id INTEGER,
@@ -15,4 +14,10 @@ SQL_CREATE_CAMPUSES_STUDENTS <<-SQL
     FOREIGN KEY (campus_id) REFERENCES campuses(id)
   )
 SQL
+
+SQL_ALLOCATIONS = <<-SQL
+    SELECT s.name student, c.name campus FROM campuses c, students s, campuses_students cs
+      WHERE c.id = cs.campus_id
+      AND   cs.student_id = s.id
+  SQL
 
